ed6e37bffcaf414de1235cbd10ca5e513ee89b5e
Cynthia <29832823+CynthiaLixinLee@users.noreply.github.com>
2021-02-25 06:37:16 -0500

    Removed jQuery code, refactored temperature conversion

--- js/index.js
+++ js/index.js
@@ -1,38 +1,58 @@
-$(document).ready(function(){
+const btnF = document.querySelector(".fahrenheit");
+const btnC = document.querySelector(".celcius");
+const toggleTemp = document.querySelectorAll(".toggle-temp");
+const temp = document.querySelector(".temp");
+let tempInF;
+
+document.addEventListener("DOMContentLoaded", function () {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {  
+    navigator.geolocation.getCurrentPosition(function (position) {
       var lat = position.coords.latitude;
       var lon = position.coords.longitude;
       getWeather(lat, lon);
     });
   } else {
-  $(".weather").html("Geolocation is not supported by this browser.")
+    document.querySelector(".weather").textContent =
+      "Geolocation is not supported by this browser.";
   }
 });
 
+// Retrive weather info with OpenWeatherMap API
 function getWeather(lat, lon) {
-  $.getJSON("https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=" + lat + "&lon=" + lon + "&APPID=8f5da5a3c95b945d1b9119efc8bb164b", function (data) {
-    var json = JSON.parse(JSON.stringify(data));
-    $("#city").html(json.name + ", ");
-    $("#country").html(json.sys.country);
-    $("#description").html(json.weather[0].description);
-    $("#temperature").html(Math.round(json.main.temp));
-    document.getElementById("icon").src = "http://openweathermap.org/img/w/" + (json.weather[0].icon) +".png"
-  });
-};
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&APPID=8f5da5a3c95b945d1b9119efc8bb164b"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      document.querySelector("#city").textContent = data.name + ",";
+      document.querySelector("#country").textContent = data.sys.country;
+      document.querySelector(".description").textContent =
+        data.weather[0].description;
+      tempInF = Math.round(data.main.temp);
+      temp.textContent = tempInF;
+      document.querySelector(".icon").src =
+        "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    })
+    .catch(function () {
+      document.querySelector(".block").textContent =
+        "Could not retrieve weather info.";
+    });
+}
 
-$("#btnFahrenheit").click(function() {
-  if ($("#btnFahrenheit").hasClass("lighten")) {
-    $("#btnFahrenheit").removeClass("lighten");
-    $("#btnCelsius").addClass("lighten");
-    $("#temperature").html(Math.round((parseInt($("#temperature").text()) * 1.8) + 32));
-  }
-})
+toggleTemp.forEach((button) => {
+  button.addEventListener("click", convertTemp);
+});
 
-$("#btnCelsius").click(function() {
-  if ($("#btnCelsius").hasClass("lighten")) {
-    $("#btnCelsius").removeClass("lighten");
-    $("#btnFahrenheit").addClass("lighten");
-    $("#temperature").html(Math.round((parseInt($("#temperature").text()) - 32)/ 1.8));
-  }
-})
+// Convert between Celcius and Fahrenheit
+function convertTemp(e) {
+  btnF.classList.contains("lighten")
+    ? (temp.textContent = tempInF)
+    : (temp.textContent = Math.round((tempInF - 32) / 1.8));
+  toggleTemp.forEach((button) => button.classList.toggle("lighten"));
+}
